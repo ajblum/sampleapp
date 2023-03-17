c762737d494f9f81678eb324d1511dc6712d6968
Student User <student@workstation.lab.example.com>
2023-03-17 07:06:39 -0400

    added code to try a pg database connection

--- app-src/app.js
+++ app-src/app.js
@@ -1,10 +1,19 @@
 // from https://nodejs.org/en/docs/guides/getting-started-guide/
 
 const http = require('http');
+const { Client } = require('pg');
 
 //const hostname = '127.0.0.1';
 const hostname = '0.0.0.0';
 const port = 3000;
+
+//getting pg database connection details
+
+const pguser = process.env.PG_USER || 'myuser';
+const pghost = process.env.PG_HOST || 'myhost';
+const pgdb = process.env.PG_DB || 'mydb';
+const pgpass = process.env.PG_PASS || 'mypass';
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,7 +21,23 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
+
+const client = new Client({
+  user: pguser,
+  host: pghost,
+  database: pgdb,
+  password: pgpass,
+  port: 5432,
+});
+
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to the pg database!");
 });
 
